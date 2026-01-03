f001934dfd5a08751885a4e02d91c3710f1829e9
MrGallen <25792128+MrGallen@users.noreply.github.com>
2026-01-03 17:19:59 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='mb_detect', 
-    version='0.1.1',
+    version='1.0.0',
     description='A smart serial port detector for BBC micro:bit',
     long_description=README,
     long_description_content_type="text/markdown",
