18a45444d47fbe43e6d06ce36adbf44eac2a2350
MrGallen <25792128+MrGallen@users.noreply.github.com>
2026-01-03 11:54:48 +0000

    Create setup.py for mb_detect package
    Initial setup configuration for the mb_detect package.

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name='mb_detect', 
+    version='0.1.0',
+    description='A smart serial port detector for BBC micro:bit',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/egallen/mb_detect", # Update this if you have a github
+    author="Eoin Gallen",
+    license="MIT",
+    py_modules=['mb_detect'],
+    install_requires=['pyserial'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
