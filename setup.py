506db7578840caa67de79aa7b9e7002f28d65f75
MrGallen <25792128+MrGallen@users.noreply.github.com>
2026-01-03 17:16:17 +0000

    Downgrade version from 0.1.2 to 0.1.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name='mb_detect', 
-    version='0.1.2',
+    version='0.1.1',
     description='A smart serial port detector for BBC micro:bit',
     long_description=README,
     long_description_content_type="text/markdown",
